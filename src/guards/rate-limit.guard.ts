30ec1cd29587603a96d8d701020b98d9e9b5c383
Warrenww <warrenpig@gmail.com>
2023-12-25 17:43:54 +0800

    Add rate limit check and update request counts for IP and user

--- src/guards/rate-limit.guard.ts
+++ src/guards/rate-limit.guard.ts
@@ -1,10 +1,13 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Cache } from 'cache-manager';
+import { randomUUID } from 'crypto';
 import {
   Injectable,
   CanActivate,
   ExecutionContext,
   Inject,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 
 @Injectable()
@@ -12,16 +15,43 @@ export class RateLimitGuard implements CanActivate {
   constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const value = await this.cacheManager.get('key');
-    console.log(value);
 
     // get user ip from request
     const ip: string = request.ip;
-    console.log(ip);
+    const ipKey = `ip:${ip}`;
 
     // get the user from the request query
     const user: number = request.query.user;
-    console.log(user);
+    const userKey = `user:${user}`;
+
+    // get the request counts for the IP and user
+    const ipCount =
+      (await this.cacheManager.store.keys(`${ipKey}-*`))?.length || 0;
+    const userCount =
+      (await this.cacheManager.store.keys(`${userKey}-*`))?.length || 0;
+
+    // check the request counts
+    if (ipCount >= 10 || userCount >= 5) {
+      throw new HttpException(
+        {
+          ip: ipCount,
+          id: userCount,
+        },
+        HttpStatus.TOO_MANY_REQUESTS,
+      );
+    }
+
+    // update the request counts
+    await this.cacheManager.set(
+      `${ipKey}-${randomUUID()}`,
+      new Date().getTime(),
+      { ttl: 60 } as any,
+    );
+    await this.cacheManager.set(
+      `${userKey}-${randomUUID()}`,
+      new Date().getTime(),
+      { ttl: 60 } as any,
+    );
 
     return true;
   }
